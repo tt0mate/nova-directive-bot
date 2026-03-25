0fa56d14a5a5ff9ebe8b4a5b63c7b21014c2b5e8
tt0mate <56605499-tt0mate@users.noreply.replit.com>
2026-03-25 17:38:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+token = os.getenv('DISCORD_TOKEN')
+if not token:
+    print('Error: DISCORD_TOKEN environment variable not set.')
+else:
+    client.run(token)
